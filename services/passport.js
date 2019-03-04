49eb09682371a3be30a61bb7c0e186bfac472888
kamran <kamran.mujtaba@upskillpeople.com>
2019-03-04 17:00:25 +0000

    handled promises

--- services/passport.js
+++ services/passport.js
@@ -10,9 +10,17 @@ passport.use(new googleStrategy({
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
-        new User({
-            googleId: profile.id
-        }).save();
+        User.findOne({ googleId: profile.id })
+            .then(existingUser => {
+                if (existingUser) {
+                    done(null, existingUser);   
+                } else {
+                    new User({ googleId: profile.id })
+                        .save()
+                        .then(user => done(null, user))
+                        .catch(err => console.log(err));
+                }
+            }).catch(err => console.log(err));
     })
 
 );
