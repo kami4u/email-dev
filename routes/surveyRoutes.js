4502e845deecab33e1c5c4bf6ced01e8ef665ffb
kamran <kamran.mujtaba@upskillpeople.com>
2019-03-08 16:52:46 +0000

    working with redux-form

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,7 +7,7 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-    app.get('/api/thanks', (req, res) => {
+    app.get('/api/surveys/thanks', (req, res) => {
         res.send('Thanks for voting');
     });
     app.post('/api/surveys', requireLogin, requireCredits,  async (req, res) => {
